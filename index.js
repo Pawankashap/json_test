dbab6624a4d0729057077825a9afd1928e8741b9
Fnu Pawan Kumar <pawan.3940@gmail.com>
2025-05-20 17:00:57 -0700

    install json-server

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ function getdata(){
         console.log(typeof animal[0].id)
 })
 }
+
+getdata()
+addData()
+
+function addData() {
+    
+    fetch("http://localhost:3000/animals", {
+        method: 'POST',
+        headers: { // <-- lowercase 'headers'
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: "test name",
+            description: "test description"
+        }) // Exclude 'id' here!
+    })
+    .then(res => res.json())
+    .then(animal => console.log(animal))
+}
